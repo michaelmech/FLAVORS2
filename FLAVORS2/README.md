5375cd5fd716260c7cb2d610f034b9c74107bf5b
michaelmech <56457392+michaelmech@users.noreply.github.com>
2025-10-11 12:31:54 -0700

    Delete FLAVORS2 directory

--- FLAVORS2/README.md
+++
@@ -1,2 +0,0 @@
-# FLAVORS2
-Fast and Lightweight Assessment of Variables for Optimally-Reduced Subsets towards Feature Learning Automation with Variable-Objective, Resource Scheduling
--- FLAVORS2/src/flavors2/__version__.py
+++
@@ -1,7 +0,0 @@
-"""Version information for the flavors2 package.
-
-This file defines the ``__version__`` attribute, which follows semantic
-versioning. Update the version string whenever you make a release.
-"""
-
-__version__ = "0.1.0"
